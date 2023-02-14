12f2fe5f006ddefd6f05c5f122dfabd8486a35ee
IsabelCollado <icolladoherrera@gmail.com>
2023-02-14 11:34:11 +0100

    Add js

--- js/main.js
+++ js/main.js
@@ -1,55 +1,56 @@
-'use strict'
+"use strict";
 
 //constantes
- 
-const inpEle= document.querySelector('.js-inpnumber');
-const btn= document.querySelector('.js-btn');
-const clueEle= document.querySelector('.js-clue');
-const trieEle= document.querySelector('.js-tried');
 
-console.log('holiiii')
+const inpEle = document.querySelector(".js-inpnumber");
+const btn = document.querySelector(".js-btn");
+const clueEle = document.querySelector(".js-clue");
+const trieElem = document.querySelector(".js-tried");
 
+console.log("holiiii");
 
 //funciones
 
 function getRandomNumber(max) {
-    
-    const numberRandom =  Math.ceil(Math.random() * max);
-    return numberRandom;
+  const numberRandom = Math.ceil(Math.random() * max);
+  return numberRandom;
+}
 
-    }
-    function useRandomNumber() {
-    const userNumber = numberSelection.value; 
-    const randomNumber= getRandomNumber(100);
-        if (inp.value === RandomNumber) {
-        clueEle.innerHTML = 'Has ganado campeona!!!';
+function useRandomNumber() {
+  //const userNumber = numberSelection.value;
 
-        } else if (inp.value <  RandomNumber &&  inpValue >0){
-        clueEle.innerHTML = 'Demasiado bajo';
+  const randomNumber = getRandomNumber(100);
 
-        }else if (inp.value > RandomNumber && inpValue < 101){
-        clueEle.innerHTML = 'Demasiado alto';
+  console.log(randomNumber);
+  if (inpEle.value === randomNumber) {
+    clueEle.innerHTML = "Has ganado campeona!!!";
+  } else if (inpEle.value < randomNumber && inpEle.value > 0) {
+    clueEle.innerHTML = "Demasiado bajo";
+  } else if (inpEle.value > randomNumber && inpEle.value < 101) {
+    clueEle.innerHTML = "Demasiado alto";
+  }
+  if (inpEle.value > 100 || inpEle.value < -1) {
+    clueEle.innerHTML = "El número debe estar entre 1 y 100";
+  }
+}
 
-        }if(inpValue > 100 || inputValue < -1){
-        clueEle.innerHTML = 'El número debe estar entre 1 y 100';
-        console
-    }
-    }
+const countAttemps = getRandomNumber(100);
+let attempts = 1;
 
-    const countAttemps = getRandomNumber(100);
-    let attempts = 1;
-    function trieEle() {
-        const totalTries = attempts++;
+function trieEle() {
+  const totalTries = attempts++;
 
-        tryUser.innerHTML = `Número de intentos: ${totalTries}`;
-    } 
+  tryUser.innerHTML = `Número de intentos: ${totalTries}`;
+}
 
 //eventos
 
 function handleClickBtn(ev) {
-    ev.preventDefault();
-    useRandomNumber();
-    trieEle();
+  ev.preventDefault();
+
+  useRandomNumber();
+
+  trieEle();
 }
 
-btn.addEventListener("click" , handleClickBtn)
+btn.addEventListener("click", handleClickBtn);
