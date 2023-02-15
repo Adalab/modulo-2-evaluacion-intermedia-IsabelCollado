22368dabd2a40468901b15337fdb51f22d85b692
IsabelCollado <icolladoherrera@gmail.com>
2023-02-15 17:57:18 +0100

    Add the constants

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 /*  1- Crear HTML, CSS y enlazar JS. OK!
     2- Escribir la estructura de HTML: necesitamos un título, 1 label, 2 input (1-bottom) y 2 párrafos ( 1 para la pista y el segundo para los intentos). OK!
-    3- Crear las constantes de html en js.
+    3- Crear las constantes de html en js. OK!
     4- Generar número Random cuando carga la página, solamente.
     5- Escuchar evento sobre el boton de 'prueba (click).
     6- Validar número de usuaria (1-100).
@@ -11,3 +11,10 @@
     9- Contador.
     10- Pintar los intentos.
 */
+
+//CONSTANTES
+
+const enterNumber = document.querySelector(".js_enterNumber");
+const btn = document.querySelector(".js_btn");
+const clueElement = document.querySelector(".js_clue");
+const triesElement = document.querySelector(".js_tries");
