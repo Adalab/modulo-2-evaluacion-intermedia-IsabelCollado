856f6e1fcd97742eef1adc9d549963315321ebd4
IsabelCollado <icolladoherrera@gmail.com>
2023-02-15 17:51:50 +0100

    add html and css structure

---
+++ js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/*  1- Crear HTML, CSS y enlazar JS. OK!
+    2- Escribir la estructura de HTML: necesitamos un título, 1 label, 2 input (1-bottom) y 2 párrafos ( 1 para la pista y el segundo para los intentos). OK!
+    3- Crear las constantes de html en js.
+    4- Generar número Random cuando carga la página, solamente.
+    5- Escuchar evento sobre el boton de 'prueba (click).
+    6- Validar número de usuaria (1-100).
+    7- Comparación del número de la pagina con el de la ususaria.
+    8- La pista.
+    9- Contador.
+    10- Pintar los intentos.
+*/
