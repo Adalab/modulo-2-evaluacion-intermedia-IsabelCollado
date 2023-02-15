92f31be8ce5e891cb41378fd2b65456a0604141f
IsabelCollado <icolladoherrera@gmail.com>
2023-02-15 18:12:13 +0100

    Add the event listener on the button

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
     2- Escribir la estructura de HTML: necesitamos un título, 1 label, 2 input (1-bottom) y 2 párrafos ( 1 para la pista y el segundo para los intentos). OK!
     3- Crear las constantes de html en js. OK!
     4- Generar número Random cuando carga la página, solamente. OK!
-    5- Escuchar evento sobre el boton de 'prueba (click).
+    5- Escuchar evento sobre el boton de 'prueba' (click). OK!
     6- Validar número de usuaria (1-100).
     7- Comparación del número de la pagina con el de la ususaria.
     8- La pista.
@@ -24,3 +24,11 @@ function getRandomNumber(max) {
 }
 
 const randomNumber = getRandomNumber(100);
+
+function handleclickbtn(ev) {
+  ev.preventDefault();
+}
+
+//EVENTOS
+
+btn.addEventListener("click", handleclickbtn);
